a1a7b6ad88af1fa56bc1dc0415160b08fc625753
Ricardo <panswer.rjmm@gmail.com>
2020-03-31 19:21:28 -0400

    video 3 primera pagina

---
+++ cursoPHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print "Bienvenidos al curso de PHP <br>";
+        print "Hola alumnos <br>";
+        print "Hasta el proximo video";
+    ?>
+</body>
+</html>
