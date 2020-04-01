4c3f2e80644a5d695c84117d717596a7f38f0d04
Ricardo <panswer.rjmm@gmail.com>
2020-04-01 13:07:40 -0400

    Comparacion de string

--- cursoPHP/index.php
+++ cursoPHP/index.php
@@ -3,19 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        .resaltar{
+            color: #F00;
+            font-weight:bold;
+        }
+    </style>
 </head>
 <body>
     <?php
-        function incrementeVariable(){
-            // static permite conservar el valor
-            static $contador=0;
-            $contador++;
-            echo $contador . "<br>";
+        /* echo "<p class='resaltar'>Esto es un ejemplo de frase</p>";
+        echo '<p class="resaltar">Esto es un ejemplo de frase</p>';
+        echo "<p class=\"resaltar\">Esto es un ejemplo de frase</p>"; */
+        /* 
+            strcmp => compara strings con mayusculas o minusculas
+            strcasecmp => compara string sin tomar encuenta si son mayusculas o minusculas
+        */
+        $variable1="casa";
+        $variable2="CASA";
+
+        // $resultado=strcmp($variable1,$variable2); //retorna 1 si no son iguales y 0 si son iguales
+        $resultado=strcasecmp($variable1,$variable2);
+
+        // echo "El resultado es $resultado";
+        if($resultado){
+            echo "No coinciden";
+        }else{
+            echo "Coinciden";
         }
-        incrementeVariable();
-        incrementeVariable();
-        incrementeVariable();
-        incrementeVariable();
     ?>
 </body>
 </html>
