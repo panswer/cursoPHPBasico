8b50a012c2859fa8457c7203bba2440b61d45e0d
Ricardo <panswer.rjmm@gmail.com>
2020-04-02 18:32:06 -0400

    Programacion Orientada a Objetos III

--- cursoPHP/index.php
+++ cursoPHP/index.php
@@ -10,35 +10,49 @@
     <!-- Codigo PHP -->
     <?php
         /* Clase */
-         class Coche{
-             var $ruedas;
-             var $color;
-             var $motor;
-             /* Constructor */
-             function Coche(){
-                $this->ruedas=4;
-                $this->color="";
-                $this->motor=1600;
-             }
-             function Arrancar(){
-                 echo "Estoy arrancando<br>";
-             }
-             function Girar(){
-                 echo "Estoy girando<br>";
-             }
-             function Frenar(){
-                 echo "Estoy frenando<br>";
-             }
-         }
-
-         /* Instancia */
-        $renault = new Coche();
+        // class Coche{
+        //     var $ruedas;
+        //     var $color;
+        //     var $motor;
+        //     /* Constructor */
+        //     function Coche(){
+        //         $this->ruedas=4;
+        //         $this->color="";
+        //         $this->motor=1600;
+        //      }
+        //     function Arrancar(){
+        //         echo "Estoy arrancando<br>";
+        //     }
+        //     function Girar(){
+        //         echo "Estoy girando<br>";
+        //     }
+        //     function Frenar(){
+        //         echo "Estoy frenando<br>";
+        //     }
+        //     function Establece_Color($color_coche,$nombre_coche){
+        //         $this->color=$color_coche;
+        //         echo ("El color de $nombre_coche es: " . $this->color . "<br>");
+        //     }
+        // }
+        /* Instancia */
+        /* $renault = new Coche();
         $mazda = new Coche();
         $seat = new Coche();
 
-        $mazda->Girar();
+        $renault->Establece_Color("Rojo","Renault");
+        $seat->Establece_Color("Azul","Seat"); */
 
-        echo $mazda->ruedas;
+        // $mazda->Girar();
+
+        // echo $mazda->ruedas;
+        include("./vehiculos.php");
+        /* Si el constructor no recibe parametros no es necesario los parametros */
+        $mazda = new Coche();
+
+        $pegaso = new Camion();
+
+        echo "El mazda tiene " . $mazda->ruedas . " ruedas <br>";
+        echo "El pegaso tiene " . $pegaso->ruedas . " ruedas <br>";
     ?>
 </body>
 
