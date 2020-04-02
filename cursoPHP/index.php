a81f4062436a8b98293e267f1282e87f69da6e7f
Ricardo <panswer.rjmm@gmail.com>
2020-04-02 14:09:16 -0400

    Funciones matematicas y casting (parsear en Javascript)

--- cursoPHP/index.php
+++ cursoPHP/index.php
@@ -4,53 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <style>
-        .resultado{
-            color: #F00;
-            font-weight: bold;
-            font-size: 32px;
-        }
-    </style>
-
 </head>
 
 <body>
-    <p>&nbsp;</p>
-    <form action="" name="form1" method="post">
-        <p>
-            <label for="num1"></label>
-            <input type="text" name="num1" id="num1">
-            <label for="num2"></label>
-            <input type="text" name="num2" id="num2">
-            <label for="operation"></label>
-            <select name="operation" id="operation">
-                <option>Suma</option>
-                <option>Resta</option>
-                <option>Multiplicacion</option>
-                <option>Division</option>
-                <option>Modulo</option>
-                <option>Incremento</option>
-                <option>Decremento</option>
-            </select>
-        </p>
-        <p>
-            <input type="submit" value="Enviar" id="button" name="button" onClick="prueba">
-        </p>
-    </form>
-    <p>&nbsp;</p>
     <!-- Codigo PHP -->
     <?php
-        include("calculadora.php");
+        // rand() debuelve un valor aleatorio
+        // Sin argumento debuelve un numero aleatorio
+        // $num1=rand();
+        // Con argumento debuelve un numero aleatorio comprendido entre los valores indicados. Primer parametro el valor minimo, segundo parametro valor maximo.
+        // $num1=rand(50,100);
+        // echo "El numero es: $num1";
+        // $num1=pow(5,3);
+        /* 
+            PHP cambia el tipo de dato dependiendo de el valor a guardar
+        */
+        $num1="5";
 
-        if(isset($_POST["button"])){
-            $numero1=$_POST["num1"];
-            $numero2=$_POST['num2'];
-            /* El select toma primero el dato de value y si este no es atributo en la option busca su texto interno */
-            $operacion=$_POST["operation"];
-            $resultado;
-            Calcular($operacion,$numero1,$numero2);
-            echo "<p class='resultado'>El resultado es: $resultado</p>";
-        }
+        $resultado=(int)($num1);
+        
+        echo "El resultado es: " . round($num1,2);
     ?>
 </body>
 
