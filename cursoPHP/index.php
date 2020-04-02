1ab94f1b6e437e090adba914638c30b29094029e
Ricardo <panswer.rjmm@gmail.com>
2020-04-02 18:10:24 -0400

    Programacion Orientada a Objetos II

--- cursoPHP/index.php
+++ cursoPHP/index.php
@@ -10,12 +10,35 @@
     <!-- Codigo PHP -->
     <?php
         /* Clase */
-         class Coche{}
+         class Coche{
+             var $ruedas;
+             var $color;
+             var $motor;
+             /* Constructor */
+             function Coche(){
+                $this->ruedas=4;
+                $this->color="";
+                $this->motor=1600;
+             }
+             function Arrancar(){
+                 echo "Estoy arrancando<br>";
+             }
+             function Girar(){
+                 echo "Estoy girando<br>";
+             }
+             function Frenar(){
+                 echo "Estoy frenando<br>";
+             }
+         }
 
          /* Instancia */
         $renault = new Coche();
         $mazda = new Coche();
         $seat = new Coche();
+
+        $mazda->Girar();
+
+        echo $mazda->ruedas;
     ?>
 </body>
 
