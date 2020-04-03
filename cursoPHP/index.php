9e7d5a0e35e926acb8e4ecb0a77eeaef1e6c20d7
Ricardo <panswer.rjmm@gmail.com>
2020-04-02 22:22:06 -0400

    Arreglos III arreglo multidimensional

--- cursoPHP/index.php
+++ cursoPHP/index.php
@@ -9,56 +9,30 @@
 <body>
     <!-- Codigo PHP -->
     <?php
-        /* Array indexado */
-        $semana[]="Lunes";
-        $semana[]="Martes";
-        $semana[]="Miercoles";
-        $semana[]="Jueves";
-        /* 
-            Se puede rellenar array sin utilizar indixe (Su efecto es como un push). De igual forma se pueden ingresar valores indicando el index, teniendo en cuenta que los indices que no sean definidos marcaran error si se llega a tocar alguno
-        */
-        /* Otra forma de crear un array indexado */
-        // $semana=array("Lunes","Martes","Miercoles");
-        // echo $semana[2];
+        /* Una dimension */
+        $alimentos = array( "fruta"=>array("tropical"=>"kiwi",
+                                            "citrico"=>"mandarina",
+                                            "otros"=>"manzana"
+                                        ),
+                            "leche"=>array("animal"=>"vaca",
+                                            "vegetal"=>"coco"
+                                        ),
+                            "carne"=>array("vacuno"=>"lomo",
+                                            "porcino"=>"pata"
+                                        )
+                        );
 
-        /* Array asociativo */
-        // $datos=array("Nombre"=>"Ricardo","Apellido"=>"Mejias","Edad"=>22);
-        // echo $datos["Apellido"];
-        // $datos="Martin";
+        // echo $alimentos["carne"]["vacuno"];
+        /* Recorrer un arreglo de mas de una dimension */
+        /* foreach($alimentos as $clave_alim=>$alim){
+            echo "$clave_alim:<br>";
+            while(list($clave,$valor)=each($alim)){
+                echo "$clave = $valor<br>";
+            }
+            echo "<br>";
+        } */
 
-        // if(is_array($datos)){
-        //     echo "Es un array";
-        // }else{
-        //     echo "No es un array";
-        // }
-        
-        /* Recorrer array asociativo */
-        // foreach($datos as $clave=>$valor){
-        //     echo "A $clave le corresponde $valor <br>";
-        // }
-
-        /* Recorrer array indexado */
-        // for($i=0;$i<count($semana);$i++){
-        //     echo $semana[$i] . "<br>";
-        // }
-
-        /* Agregar un elemento en array indexado */
-        // $semana[]="Viernes";
-        // for($i=0;$i<count($semana);$i++){
-        //     echo $semana[$i] . "<br>";
-        // }
-
-        /* Agregar un elemento en array asociativo */
-        // $datos["Pais"]="Venezuela";
-        // foreach($datos as $clave=>$valor){
-        //     echo "El $clave tiene $valor <br>";
-        // }
-
-        /* ordenar elemento de array indexado */
-        sort($semana);
-        for($i=0;$i<count($semana);$i++){
-            echo $semana[$i] . "<br>";
-        }
+        echo var_dump($alimentos);
     ?>
 </body>
 
