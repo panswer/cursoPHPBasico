198b6c740a70c20595f6f39f92db28d2fa4a8e5d
Ricardo <panswer.rjmm@gmail.com>
2020-04-02 21:51:08 -0400

    Arrays II

--- cursoPHP/index.php
+++ cursoPHP/index.php
@@ -10,9 +10,10 @@
     <!-- Codigo PHP -->
     <?php
         /* Array indexado */
-        // $semana[]="Lunes";
-        // $semana[]="Martes";
-        // $semana[]="Miercoles";
+        $semana[]="Lunes";
+        $semana[]="Martes";
+        $semana[]="Miercoles";
+        $semana[]="Jueves";
         /* 
             Se puede rellenar array sin utilizar indixe (Su efecto es como un push). De igual forma se pueden ingresar valores indicando el index, teniendo en cuenta que los indices que no sean definidos marcaran error si se llega a tocar alguno
         */
@@ -21,8 +22,43 @@
         // echo $semana[2];
 
         /* Array asociativo */
-        $datos=array("Nombre"=>"Ricardo","Apellido"=>"Mejias","Edad"=>22);
-        echo $datos["Apellido"];
+        // $datos=array("Nombre"=>"Ricardo","Apellido"=>"Mejias","Edad"=>22);
+        // echo $datos["Apellido"];
+        // $datos="Martin";
+
+        // if(is_array($datos)){
+        //     echo "Es un array";
+        // }else{
+        //     echo "No es un array";
+        // }
+        
+        /* Recorrer array asociativo */
+        // foreach($datos as $clave=>$valor){
+        //     echo "A $clave le corresponde $valor <br>";
+        // }
+
+        /* Recorrer array indexado */
+        // for($i=0;$i<count($semana);$i++){
+        //     echo $semana[$i] . "<br>";
+        // }
+
+        /* Agregar un elemento en array indexado */
+        // $semana[]="Viernes";
+        // for($i=0;$i<count($semana);$i++){
+        //     echo $semana[$i] . "<br>";
+        // }
+
+        /* Agregar un elemento en array asociativo */
+        // $datos["Pais"]="Venezuela";
+        // foreach($datos as $clave=>$valor){
+        //     echo "El $clave tiene $valor <br>";
+        // }
+
+        /* ordenar elemento de array indexado */
+        sort($semana);
+        for($i=0;$i<count($semana);$i++){
+            echo $semana[$i] . "<br>";
+        }
     ?>
 </body>
 
